64ddc0a344e7bd7ee0b06bde4eb22c4fceead7e1
0613jyh <0613jyh@naver.com>
2023-04-14 20:32:24 +0900

    chap02

---
+++ Chap02/error4/error4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hey!\n");
+	printf("Good Morning!");
+
+	// 예제에서는 \n을 안 들어가지만, 논리오류가 아니라 우리가 원하는 프로그램을 만들 때 오타나, 원하는 프로그램이 안 만들어지는 경우가 있으므로 오타검증은 신중히 해야한다.
+	return 0;
+
+}
+
